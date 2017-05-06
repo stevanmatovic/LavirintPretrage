e500c216d61c31a8a2c9a83587f0d48e7529eaa0
stevanmatovic <stevan.matovic95@gmail.com>
2017-05-06 12:01:23 +0200

    TO DO 1 i 2 i BFS uradjeni

--- Lavirint/State.cs
+++ Lavirint/State.cs
@@ -10,6 +10,7 @@ namespace Lavirint
         State parent;
         public int markI, markJ; //vrsta i kolona
         public double cost;
+        private int[,] moves = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
         public State sledeceStanje(int markI, int markJ)
         {
             State rez = new State();
@@ -26,7 +27,33 @@ namespace Lavirint
             //TODO1: Implementirati metodu tako da odredjuje dozvoljeno kretanje u lavirintu
             //TODO2: Prosiriti metodu tako da se ne moze prolaziti kroz sive kutije
             List<State> rez = new List<State>();
+
+            for (int ind = 0; ind < moves.GetLength(0); ind++)
+            {
+                int newI = this.markI + moves[ind, 0];
+                int newJ = this.markJ + moves[ind, 1];
+
+                if (isAllowedState(newI, newJ))
+                {
+                    rez.Add(sledeceStanje(newI, newJ));
+                }
+            }
+
             return rez;
+        }
+
+        private bool isAllowedState(int newI, int newJ)
+        {
+            if (newI < 0 || newJ < 0)
+                return false;
+            if (newI >= Main.brojVrsta || newJ >= Main.brojKolona)
+                return false;
+            if (lavirint[newI, newJ] == 1)      //ukoliko je siv polje
+                return false;
+
+            return true;
+            
+
         }
 
         public override int GetHashCode()
